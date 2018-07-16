e35309ee5c1fb35b22f45bd63cba28a83f412b5b
Anurag Dadheech <anurag.dadheech@myntra.com>
2018-07-16 18:26:09 +0530

    Home screen with textInput

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Image,
+	TouchableWithoutFeedback,
+} from 'react-native';
+
+let myntraLogo = "https://assets.myntassets.com/assets/images/retaillabs/2018/7/16/3ec24395-fec9-4cf9-9440-9118de40555a1531742032871-myntra-logo.png";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>HELLO WORLD!</Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { text: '' };
+		this.handleSearchPress = this.handleSearchPress.bind(this)
+	}
+
+	handleSearchPress() {
+		alert(this.state.text);
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image source={{ uri: myntraLogo }} style={ styles.logo } />
+				<Text style={[styles.infoText, styles.medium]}>SEARCH PRODUCTS ON <Text style={styles.regular}>MYNTRA</Text></Text>
+				<View style={ styles.inputContainer }>
+					<TextInput
+						style={ styles.input }
+						onChangeText={ text => this.setState({text}) }
+						value={ this.state.text }
+					/>
+				</View>
+				<TouchableWithoutFeedback onPress={ this.handleSearchPress }>
+					<View style={ styles.nextBtn }>
+						<Text style={styles.nextBtnText}>GO</Text>
+					</View>
+				</TouchableWithoutFeedback>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		marginTop: 50,
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: 25,
+		marginHorizontal: 40,
+		backgroundColor: '#f5f5f6',
+		borderColor: '#d5d6d9',
+		borderWidth: 0.5,
+		height: 45,
+		borderRadius: 4,
+		alignSelf: 'stretch',
+		paddingHorizontal: 15,
+	},
+	input: {
+		flex: 1,
+		color: '#161925',
+		fontSize: 20,
+	},
+	nextBtn: {
+		marginTop: 50,
+		height: 40,
+		width: 100,
+		backgroundColor: '#FF3F6C',
+		justifyContent: 'center',
+		borderRadius: 50,
+	},
+	nextBtnText: {
+		fontWeight: 'bold',
+		fontSize: 13,
+		color: '#ffffff',
+		alignSelf: 'center',
+	},
+	logo: {
+		width: 100,
+		height: 100
+	},
+	infoText: {
+		fontSize: 11,
+		letterSpacing : 1.5,
+		marginTop: 80,
+	},
+	medium: {
+		color: '#161925',
+	},
+	regular: {
+		color: '#94989f',
+	},
 });
