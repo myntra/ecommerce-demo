b7487bb83a462e5ab746029eb314616c2455a31e
Anurag Dadheech <anurag.dadheech@myntra.com>
2020-07-01 11:13:42 +0530

    Fixes

--- App.js
+++ App.js
@@ -1,45 +1,27 @@
-import React from 'react';
-import {StatusBar, Platform, View} from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import LandingPage from './src/apps/landingpage';
 import Search from './src/apps/search';
 import PDP from './src/apps/pdp';
-
-export default class App extends React.Component {
-	
-	render() {
-		return (
-			<View style={{ flex: 1, marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight }}>
-				<RootNavigator />
-			</View>
-		);
-	}
-}
 
 const RootNavigator = createStackNavigator(
 	{
 		Home: {
 			screen: LandingPage,
 			navigationOptions: () => ({
-				header: null,
+				headerShown: false,
 			}),
 		},
 		Search: {
 			screen: Search,
 			navigationOptions: () => ({
 				title: 'Search',
-				headerStyle:{
-					marginTop: Platform.OS === 'ios' ? 0 : -StatusBar.currentHeight,
-				},
 			}),
 		},
 		PDP: {
 			screen: PDP,
 			navigationOptions: () => ({
 				title: 'PDP',
-				headerStyle:{
-					marginTop: Platform.OS === 'ios' ? 0 : -StatusBar.currentHeight,
-				},
 			}),
 		},
 	},
@@ -47,3 +29,5 @@ const RootNavigator = createStackNavigator(
 		initialRouteName: 'Home',
 	}
 );
+
+export default createAppContainer(RootNavigator);
